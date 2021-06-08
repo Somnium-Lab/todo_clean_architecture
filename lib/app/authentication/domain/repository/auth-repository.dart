1d9a0fed2d06f7009620e2c680d88f9a267e9fce
samarjeetr2001 <61595281+samarjeetr2001@users.noreply.github.com>
2021-06-08 12:20:03 +0530

    signout usecase

--- lib/app/authentication/domain/repository/auth-repository.dart
+++ lib/app/authentication/domain/repository/auth-repository.dart
@@ -3,4 +3,9 @@ abstract class AuthRepository {
     required String email,
     required String password,
   });
+  Future<void> userSignUp({
+    required String email,
+    required String password,
+  });
+  Future<void> userSignOut();
 }
