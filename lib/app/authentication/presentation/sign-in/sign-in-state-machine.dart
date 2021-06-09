890ec0fb9b0b8100bf4ff4f1689ae8ec821a9986
Siddhesh Dandavatimath <siddhu15062000@gmail.com>
2021-06-09 18:19:07 +0530

    sign-in-state-machine-optimization

--- lib/app/authentication/presentation/sign-in/sign-in-state-machine.dart
+++ lib/app/authentication/presentation/sign-in/sign-in-state-machine.dart
@@ -1,19 +1,35 @@
 import 'package:todo_clean_architecture/core/presentation/state-machine.dart';
 
 class SignInStateMachine extends StateMachine<SignInState, SignInEvent> {
-  SignInStateMachine() : super(SignInInitializationState());
+  SignInStateMachine() : super(SignInInitState());
 
   @override
   SignInState getStateOnEvent(SignInEvent event) {
     final eventType = event.runtimeType;
     SignInState newState = getCurrentState();
     switch (eventType) {
+      case SignInClickEvent:
+        newState = new SignInLoadingState();
+        break;
+
+      case SignInErrorEvent:
+        newState = SignInErrorState();
+        break;
+
+      default:
+        throw ("Invalid State : SignInStateMachine");
     }
     return newState;
   }
 }
 
-class SignInInitializationState extends SignInState {}
+class SignInErrorState extends SignInState{}
+
+class SignInErrorEvent extends SignInEvent{}
+
+class SignInClickEvent extends SignInEvent{}
+
+class SignInInitState extends SignInState {}
 
 class SignInState {}
 
--- lib/app/authentication/presentation/sign-in/view/sign-in-view.dart
+++ lib/app/authentication/presentation/sign-in/view/sign-in-view.dart
@@ -1,1 +1,11 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
+class SignInView extends View {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+  
+}
