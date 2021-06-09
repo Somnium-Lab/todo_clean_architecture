bc7da52569b005d80954cf9ca6e7aaa3ceb832d9
Siddhesh Dandavatimath <siddhu15062000@gmail.com>
2021-06-09 18:22:03 +0530

    sign-in-view

--- lib/app/authentication/presentation/sign-in/view/sign-in-view.dart
+++ lib/app/authentication/presentation/sign-in/view/sign-in-view.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+import 'package:todo_clean_architecture/app/authentication/presentation/sign-in/sign-in-controller.dart';
 
 class SignInView extends View {
   @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    throw UnimplementedError();
-  }
-  
+  State<StatefulWidget> createState() => SignInViewState();
+}
+
+class SignInViewState
+    extends ResponsiveViewState<SignInView, SignInController> {
+  SignInViewState() : super(new SignInController());
+
+  @override
+  Widget get desktopView => throw UnimplementedError();
+
+  @override
+  Widget get mobileView => throw UnimplementedError();
+
+  @override
+  Widget get tabletView => throw UnimplementedError();
+
+  @override
+  Widget get watchView => throw UnimplementedError();
 }
